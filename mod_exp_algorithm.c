f8c9c8476b3f56d9f783017f6d8879ca29a0986a
priyanshu0503 <priyanshu625@yahoo.com>
2024-12-04 20:55:16 -0600

    Create mod_exp_algorithm.c

---
+++ mod_exp_algorithm.c
@@ -0,0 +1,9 @@
+/*********************************************************
+Header file for the modular exponentiation algorithm
+*********************************************************/
+#ifndef MOD_EXP_ALGORITHM_H
+#define MOD_EXP_ALGORITHM_H
+
+long long int fast_mod_exp(long long int base, long long int exp, long long int mod);
+
+#endif // MOD_EXP_ALGORITHM_H
